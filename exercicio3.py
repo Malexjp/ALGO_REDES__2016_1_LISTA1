a571efcaf16b0314db26340397a9c62631295fdc
Malexjp <malexjp@gmail.com>
2016-02-06 17:39:42 -0200

    Create exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,18 @@
+m = 200
+n = 250
+a = m + n
+print("O resultado de a é: %d" %a)
+o = 200
+p = 8
+b = o / p
+print("O resultado de b é: %d" %b)
+q = 200
+r = 7
+c = q * r
+print("O resultado de c é: %d" %c)
+s = 800
+t = 10
+d = s - t
+print("O resultado de d é: %d" %d)
+e = 2 ** 10
+print("O resultado de e é: %d" %e)
